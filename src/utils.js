64c7853fd951c9eb70dfe04ab3c5ae06801366c1
rahat605 <mdrahatrion2@gmail.com>
2025-12-23 12:37:00 +0600

    deplou

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,12 @@
-export const getImageUrl = (path)=>{
-    return new URL ("./assets/"+path, import.meta.url).href;
+// export const getImageUrl = (path)=>{
+//     return new URL ("./assets/"+path, import.meta.url).href;
+// };  
+
+const images = import.meta.glob("./assets/**/*.{png,jpg,jpeg,svg,webp}", {
+  eager: true,
+  import: "default",
+});
+
+export const getImageUrl = (path) => {
+  return images[`./assets/${path}`];
 };
