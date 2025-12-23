8e41472fd21be8205335d227975bae78d689a813
rahat605 <mdrahatrion2@gmail.com>
2025-12-23 17:08:24 +0600

    utils js fix

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,11 @@
-export const getImageUrl = (path)=>{
-    return new URL ("./assets/"+path, import.meta.url).href;
-};  
+// export const getImageUrl = (path)=>{
+//     return new URL ("./assets/"+path, import.meta.url).href;
+// };  
+
+const images = import.meta.glob(
+  "/src/assets/**/*.{png,jpg,jpeg,svg,webp}",
+  { eager: true, import: "default" }
+);
+
+export const getImageUrl = (path) =>
+  images[`/src/assets/${path}`];
