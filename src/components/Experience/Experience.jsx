915c5510d72af38c21bc2a47251b9ccbd6813b23
rahat605 <mdrahatrion2@gmail.com>
2025-12-23 11:58:28 +0600

    experience

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -8,19 +8,23 @@ const Experience = () => {
   return (
     <section className={styes.container} id='experience'>
         <h2 className={styes.title}>Experience</h2>
-        <div>
-          <div>
+        <div className={styes.content}>
+          <div className={styes.skills}>
             {skills.map((skill,id)=>{
-              return <div key={id}>
-                <div><img src={getImageUrl(skill.imageSrc)} alt={skill.title} /></div>
+              return <div key={id} className={styes.skill}>
+                <div className={styes.imageContainer}><img src={getImageUrl(skill.imageSrc)} alt={skill.title} /></div>
                 <p>{skill.title}</p>
               </div>
             })}
           </div>
-          <ul>
+          <ul className={styes.history}>
             {history.map((history,id)=>{
-              return <li key={id}>
-                <img src={getImageUrl(history.imageSrc)} alt="" />
+              return <li key={id} className={styes.historyItem}>
+                <img src={getImageUrl(history.imageSrc)} alt={`${history.organization} Logo`} />
+                <div className={styes.historyItemDetails}>
+                  <h3>{`${history.role}, ${history.organization}`}</h3>
+                  <p>{`${history.startDate} - ${history.endDate}`}</p>
+                </div>
               </li>
             })}
           </ul>
